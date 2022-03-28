f4f08b723201b67f510b5bd5748767bca3d9725f
Anuj Sharma <anujankur13@gmail.com>
2022-03-28 16:56:04 +0530

    Checkpoints

---
+++ src/components/service/xhr.service.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+export default {
+    get: axios.get,
+    put: axios.put,
+    post: axios.post,
+    delete: axios.delete,
+}
